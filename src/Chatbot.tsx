77983d61eff3c22753d419351c846c4cc2c8b2ca
Gerardo Leiva Diaz <a01198479@tec.mx>
2024-05-05 11:00:58 -0600

    chatbot containers

--- src/Chatbot.tsx
+++ src/Chatbot.tsx
@@ -2,7 +2,14 @@
 
 export default function Chatbot() {
     return(
-        <>
-        </>
+        <div className="page">
+            <div className="page1">
+
+            </div>
+            <div className="page2">
+
+            </div>
+
+        </div>
     )
 }
